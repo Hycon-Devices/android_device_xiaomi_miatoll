214489235941d18fa1e70888d75c6aed728e799c
HaSSaN <hasun.mohl@gmail.com>
2021-07-11 20:08:51 +0000

    Hycon Bringup

--- aosp_miatoll.mk
+++ aosp_miatoll.mk
@@ -7,20 +7,23 @@
 # Inherit from miatoll  device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
-# Inherit some common ArrowOS stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-# Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_USES_BLUR := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_USES_FACE_UNLOCK := true
+HYCON_MAINTAINER := HaSSaN
+
+#official
+HYCON_BUILD_TYPE=OFFICIAL
 
 # Device identifier
-PRODUCT_NAME := arrow_miatoll
+PRODUCT_NAME := aosp_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Miatoll
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Maintainer
-DEVICE_MAINTAINER := SonalSingh18
